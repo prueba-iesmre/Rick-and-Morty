717b59544426fa67d1fa52b7cfc301081a6642a5
Michael <micferneg@alu.edu.gva.es>
2026-04-12 14:48:50 +0200

    Ajustado estilos para episodeos y ubicaciones

--- script.js
+++ script.js
@@ -212,18 +212,24 @@ function renderCards(items, type) {
         // Insertamos la ficha con TU diseño CSS
         contenedor.innerHTML += `
             <div class="fichas">
+            <div class="header-ficha">
+            <div class="contenedor-guardado">
+            <button class="btn-guardar" onclick='guardarEnBD(${JSON.stringify(item)}, "${type}")'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path>
+                    <polyline points="17 21 17 13 7 13 7 21"></polyline>
+                    <polyline points="7 3 7 8 15 8"></polyline>
+                </svg>
+            </button>
+            </div>
                 <p class="nombre">${item.name}</p>
+                <div class="spacer"></div>
+            </div>
                 <div class="imagen">
                     <img src="${imagenUrl}">
                 </div>
                 <div class="info_fichas">
                     ${infoExtra}
-                </div>
-                    <div class="contenedor-guardado">
-                    <button class="btn-guardar" onclick='guardarEnBD(${JSON.stringify(item)}, "${type}")'>
-                    💾 Guardar ${type}
-                    </button>
-                    </div>
             </div>`;
     });
 }
